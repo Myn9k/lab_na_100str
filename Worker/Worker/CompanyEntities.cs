3fdb1651db5d70d4a9d61c590b34631b163aa71a
Myn9k <rodionpolyakov123@gmail.com>
2024-05-12 17:34:37 +0900

    Всё конец

---
+++ Worker/Worker/CompanyEntities.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using Worker.Model;
+
+namespace Workers
+{
+    public class CompanyEntities : DbContext
+    { 
+        public CompanyEntities(): base("name=CompanyEntities")
+        {
+        } 
+        public virtual DbSet<Role> Roles { get; set; }
+        public virtual DbSet<Person> Persons { get; set; }
+
+    }
+}
